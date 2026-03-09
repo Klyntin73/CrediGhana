a9e951d2e47ddba93b0b15e47a111d2f297372e2
Klyntin73 <lovelandcliton94@gmail.com>
2026-03-09 06:28:40 +0000

    fix: update build script paths to correct project location

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d "c:\Users\Lovel\Downloads\credighana-ai (1)\android"
+cd /d "c:\Users\Lovel\OneDrive\Pictures\credighana\android"
 java -jar "gradle\wrapper\gradle-wrapper.jar" assembleDebug
